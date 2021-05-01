0be041c783d4311c1ec73381d0b626fe802c756b
Diana <dianav0531@gmail.com>
2021-04-30 20:56:27 -0500

    mi primer comit

---
+++ frontendforo-main/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  api_url: 'https://backforo.herokuapp.com/api'
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  api_url: 'https://backendforo-diana.herokuapp.com//api'
+};
